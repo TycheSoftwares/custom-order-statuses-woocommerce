10f4dbc3e7ed1187ece790ff21c99771ba24ee61
Nitin Rathod <nitin@tychesoftwares.com>
2020-11-28 12:52:27 +0530

    Fixed issue #21
    In this commit, I have fixed the issue of deleting orders with custom statuses when deleting the plugin. Fix #21

--- uninstall.php
+++ uninstall.php
@@ -22,6 +22,7 @@ if ( file_exists( WP_PLUGIN_DIR . '/custom-order-statuses-for-woocommerce-pro/cu
 global $wpdb;
 
 require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+require_once 'includes/alg-wc-custom-order-statuses-functions.php';
 
 /**
  * Delete the data for the WordPress Multisite.
@@ -34,6 +35,11 @@ if ( is_multisite() ) {
 
 
 		$cos_blog_id = $cos_blog_list_value->blog_id;
+
+		/**
+		 * It stores the value of Fallback status.
+		 */
+		$fallback_delete_status = get_blog_option( $cos_blog_id, 'alg_orders_custom_statuses_fallback_delete_status', 'on-hold' );
 
 		/**
 		 * It indicates the sub site id.
@@ -53,6 +59,7 @@ if ( is_multisite() ) {
 		delete_blog_option( $cos_blog_id, 'alg_orders_custom_statuses_add_to_order_preview_actions' );
 		delete_blog_option( $cos_blog_id, 'alg_orders_custom_statuses_enable_editable' );
 		delete_blog_option( $cos_blog_id, 'alg_orders_custom_statuses_enable_paid' );
+		delete_blog_option( $cos_blog_id, 'is_statuses_migrated_to_slug' );
 
 		// Email Settings.
 		delete_blog_option( $cos_blog_id, 'alg_orders_custom_statuses_emails_enabled' );
@@ -65,6 +72,18 @@ if ( is_multisite() ) {
 		// Advanced Settings.
 		delete_blog_option( $cos_blog_id, 'alg_orders_custom_statuses_filters_priority' );
 
+		// Set default order status to all custom order status of this plugin.
+		$get_all_status = get_blog_option( $cos_blog_id, 'alg_orders_custom_statuses_array', array() );
+		if ( empty( $alg_get_custom_order_statuses_from_cpt ) ) {
+			$get_all_status = alg_get_custom_order_statuses_from_cpt( false, true );
+		}
+		if ( ! empty( $get_all_status ) ) {
+			foreach ( $get_all_status as $custom_status_key => $custom_status_id ) {
+				$wpdb->update( $cos_multisite_prefix . 'posts', array( 'post_status' => 'wc-' . $fallback_delete_status ), array( 'post_status' => $custom_status_key ) ); // phpcs:ignore
+				wp_delete_post( $custom_status_id );
+			}
+		}
+
 		// License.
 		delete_blog_option( $cos_blog_id, 'edd_license_key_cos' );
 
@@ -72,9 +91,14 @@ if ( is_multisite() ) {
 		delete_blog_option( $cos_blog_id, 'alg_orders_custom_statuses_array' );
 
 		// delete the custom order statuses.
-		$wpdb->query( $wpdb->prepare( 'DELETE FROM `%s` WHERE option_name LIKE %s', $cos_multisite_prefix . 'options', 'alg_orders_custom_status_icon_data_%' ) ); // phpcs:ignore
+		$wpdb->query( 'DELETE FROM ' . $cos_multisite_prefix . 'options WHERE option_name LIKE "alg_orders_custom_status_icon_data_%"' ); // phpcs:ignore
 	}
 } else {
+
+	/**
+	 * It stores the value of Fallback status.
+	 */
+	$fallback_delete_status = get_option( 'alg_orders_custom_statuses_fallback_delete_status', 'on-hold' );
 
 	// General Settings.
 	delete_option( 'alg_orders_custom_statuses_add_to_bulk_actions' );
@@ -89,6 +113,7 @@ if ( is_multisite() ) {
 	delete_option( 'alg_orders_custom_statuses_add_to_order_preview_actions' );
 	delete_option( 'alg_orders_custom_statuses_enable_editable' );
 	delete_option( 'alg_orders_custom_statuses_enable_paid' );
+	delete_option( 'is_statuses_migrated_to_slug' );
 
 	// Email Settings.
 	delete_option( 'alg_orders_custom_statuses_emails_enabled' );
@@ -104,11 +129,23 @@ if ( is_multisite() ) {
 	// License.
 	delete_option( 'edd_license_key_cos' );
 
+	// Set default order status to all custom order status of this plugin.
+	$get_all_status = get_option( 'alg_orders_custom_statuses_array', array() );
+	if ( empty( $alg_get_custom_order_statuses_from_cpt ) ) {
+		$get_all_status = alg_get_custom_order_statuses_from_cpt( false, true );
+	}
+	if ( ! empty( $get_all_status ) ) {
+		foreach ( $get_all_status as $custom_status_key => $custom_status_id ) {
+			$wpdb->update( $wpdb->prefix . 'posts', array( 'post_status' => 'wc-' . $fallback_delete_status ), array( 'post_status' => $custom_status_key ) ); // phpcs:ignore
+			wp_delete_post( $custom_status_id );
+		}
+	}
+
 	// custom status array.
 	delete_option( 'alg_orders_custom_statuses_array' );
 
 	// delete the custom order statuses.
-	$wpdb->query( $wpdb->prepare( 'DELETE FROM `%s` WHERE option_name LIKE %s', $wpdb->prefix . 'options', 'alg_orders_custom_status_icon_data_%' ) ); // phpcs:ignore
+	$wpdb->query( 'DELETE FROM ' . $wpdb->prefix . 'options WHERE option_name LIKE "alg_orders_custom_status_icon_data_%"' ); // phpcs:ignore
 
 }
 // Clear any cached data that has been removed.
